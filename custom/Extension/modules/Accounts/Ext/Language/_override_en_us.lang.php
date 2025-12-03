5c215ec3d64071ba9502f959f835c74488d3ad49
gadai anish <mrgadai6077@gamail.com>
2025-12-03 17:31:32 +0530

    add new field and layout

---
+++ custom/Extension/modules/Accounts/Ext/Language/_override_en_us.lang.php
@@ -0,0 +1,3 @@
+<?php
+// created: 2025-12-03 11:57:53
+$mod_strings['LBL_REG_DATE'] = 'registration date ';
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/_override_sugarfield_reg_date_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2025-12-03 11:57:53
+$dictionary['Account']['fields']['reg_date_c']['inline_edit']='1';
+$dictionary['Account']['fields']['reg_date_c']['labelValue']='registration date ';
+
+ ?>
